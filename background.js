8a062cd2ec22a012c411acb4cef2fe66e282af3c
silmoon <silmoonsone@live.com>
2023-10-03 01:47:27 +0800

    修复小问题。

--- background.js
+++ background.js
@@ -1,12 +1,22 @@
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.contextMenus.create({
-        id: "switchLanguage",
-        title: "切换到主要语言",
-        contexts: ["page"],
-        documentUrlPatterns: [
-            "*://docs.microsoft.com/*",
-            "*://learn.microsoft.com/*"
-        ]
+    chrome.storage.sync.get({
+        primaryLanguage: 'zh-cn',
+        switchMode: 'prompt',
+        autoSwitch: false,
+        manualSwitch: false,
+        promptSwitch: false
+    }, function (items) {
+        const primaryLanguage = items.primaryLanguage;
+
+        chrome.contextMenus.create({
+            id: "switchLanguage",
+            title: "切换语言到" + primaryLanguage,
+            contexts: ["page"],
+            documentUrlPatterns: [
+                "*://docs.microsoft.com/*",
+                "*://learn.microsoft.com/*"
+            ]
+        });
     });
 });
 
@@ -23,16 +33,7 @@ chrome.contextMenus.onClicked.addListener((info, tab) => {
             const languagePattern = /\/[a-z]{2}-[a-z]{2}\//;
             const newURL = tab.url.replace(languagePattern, `/${primaryLanguage}/`);
 
-            if (items.switchMode === 'auto' && items.autoSwitch) {
-                chrome.tabs.update(tab.id, { url: newURL });
-            } else if (items.switchMode === 'manual' && items.manualSwitch) {
-                chrome.tabs.update(tab.id, { url: newURL });
-            } else if (items.switchMode === 'prompt' && items.promptSwitch) {
-                const shouldSwitch = window.confirm("该页面不是你的主要语言，是否转换？");
-                if (shouldSwitch) {
-                    chrome.tabs.update(tab.id, { url: newURL });
-                }
-            }
+            chrome.tabs.update(tab.id, { url: newURL });
         });
     }
 });
